9b6867b96f5b62e4c5765121c176bfa5a5515609
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2017-12-15 19:39:58 -0300

    [ADD] Rediciona de / e /sso para /sso/admin

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^sso/admin/', admin.site.urls),
     url(r'^sso/cas/', include('cas_server.urls', namespace="cas_server")),
+    url(r'^sso/$', RedirectView.as_view(url='/sso/admin/')),
+    url(r'^$', RedirectView.as_view(url='/sso/admin/')),
 ]
