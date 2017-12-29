f6e13f46342da594cd716c4fce25b3874cf5d09f
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2017-12-29 14:45:07 -0300

    app/config/settings.py

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^sso/admin/', admin.site.urls),
-    url(r'^sso/cas/', include('cas_server.urls', namespace="cas_server")),
-    url(r'^sso/$', RedirectView.as_view(url='/sso/admin/')),
+    url(r'^%s/admin/' % settings.URL_PATH_PREFIX, admin.site.urls),
+    url(r'^%s/cas/' % settings.URL_PATH_PREFIX, include('cas_server.urls', namespace="cas_server")),
+    url(r'^%s/$' % settings.URL_PATH_PREFIX, RedirectView.as_view(url='/sso/admin/')),
     url(r'^$', RedirectView.as_view(url='/sso/admin/')),
 ]
