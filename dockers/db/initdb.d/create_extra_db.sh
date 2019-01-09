f7d9a72b6e80c8364a93b7aa67a520e1efb6fc09
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-01-09 12:10:23 -0300

    [SUB] [UPD] renaming submodules [ADD] script to change all submodules to same branch

--- dockers/db/initdb.d/create_extra_db.sh
+++ dockers/db/initdb.d/create_extra_db.sh
@@ -1,6 +1,6 @@
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_acesso
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_perfil
-createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_processo_seletivo
+createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_seletivo
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_selecao
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_integrador_ms
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_integrador_ui
