e55fb042183c915367403281e9835ec808ae1f17
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-01-15 19:16:20 -0300

    [ADD] app dashboard [SUB] dashboard

--- conf/db/create_extra_db.sh
+++ conf/db/create_extra_db.sh
@@ -4,3 +4,4 @@ createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_seletivo
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_selecao
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_integrador_ms
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_integrador_ui
+createdb -e -U $POSTGRES_USER -O $POSTGRES_USER ege_dashboard
