9d66cf2f2f5aa5f9fb3185e57f9f3268396a478c
Kelson da Costa Medeiros <kelson.costa@escolar.ifrn.edu.br>
2020-02-14 19:04:47 -0300

    [UPD] acesso to id

--- db/initdb_scripts/create_extra_db.sh
+++ db/initdb_scripts/create_extra_db.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_acesso
+createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_id
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_baggins
-createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_perfil
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_seletivo
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_selecao
 createdb -e -U $POSTGRES_USER -O $POSTGRES_USER sead_integrador_ms
